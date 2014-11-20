13eb21715d3c0d9f738ad48c7c123ec8b27eff85
Sora Havok <sora.havok@gmail.com>
2014-11-20 01:20:49 +0000

    Show how Latency Compensation works in Meteor

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -16,7 +16,8 @@ Template.postSubmit.events({
         alert('This link has already been posted');
       }
 
-      Router.go('postPage', {_id: result._id});
+      
     });
+    Router.go('postsList');
   }
 });
