48d3a0172c56ada9ac38474e98f5241c01424a23
Sora Havok <sora.havok@gmail.com>
2014-11-21 00:04:43 +0000

    Added validation for post submission

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -7,6 +7,11 @@ Template.postSubmit.events({
       title: e.target.title.value
     };
     
+    var errors = validatePost(post);
+    if(errors.title || errors.url) {
+      return Session.set('postSubmitErrors', errors);
+    }
+
     Meteor.call('postInsert', post, function(error, result){
       if (error) {
         return throwError(error.reason);
@@ -18,5 +23,18 @@ Template.postSubmit.events({
 
       Router.go('postPage', {_id: result._id});
     });
+  }
+});
+
+Template.postSubmit.created = function(){
+  Session.set('postSubmitErrors', {});
+}
+
+Template.postSubmit.helpers({
+  errorMessage: function(field) {
+    return Session.get('postSubmitErrors')[field];
+  },
+  errorClass: function(field) {
+    return !!Session.get('postSubmitErrors')[field] ? 'has-error' : '';
   }
 });
